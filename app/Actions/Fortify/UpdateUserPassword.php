f564137481d043db8539db30fbbcd1e4470db3b6
lukeraymonddowning <lukeraymonddowning@gmail.com>
2024-08-08 12:18:28 +0100

    wip

--- app/Actions/Fortify/UpdateUserPassword.php
+++ app/Actions/Fortify/UpdateUserPassword.php
@@ -14,10 +14,9 @@ class UpdateUserPassword implements UpdatesUserPasswords
     /**
      * Validate and update the user's password.
      *
-     * @param  array<string, string>  $input
+     * @param array<string, string> $input
      */
-    public function update(User $user, array $input): void
-    {
+    public function update(User $user, array $input): void {
         Validator::make($input, [
             'current_password' => ['required', 'string', 'current_password:web'],
             'password' => $this->passwordRules(),
@@ -25,9 +24,8 @@ class UpdateUserPassword implements UpdatesUserPasswords
             'current_password.current_password' => __('The provided password does not match your current password.'),
         ])->validateWithBag('updatePassword');
 
-        $user->forceFill(
-            [
-                'password' => Hash::make($input['password']),
-            ])->save();
+        $user->forceFill([
+            'password' => Hash::make($input['password']),
+        ])->save();
     }
 }
