fb0a486f3e0cc2c5f6f0c722515badb43b4c6dbb
lukeraymonddowning <lukeraymonddowning@gmail.com>
2024-08-08 13:48:14 +0100

    wip

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -19,19 +19,18 @@ class CreateNewUser implements CreatesNewUsers
      *
      * @param array<string, string> $input
      */
-    public function create(array $input): User
-    {
+    public function create(array $input): User {
         Validator::make($input, [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => $this->passwordRules(),
-            'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature() ? ['accepted', 'required'] : '',
-        ])->validate();
+            'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature() ? ['accepted', 'required'] : ''
+        ])
 
-        return DB::transaction(function () use ($input) {
-            return tap(User::create([
-                'name' => $input['name'],
-                'email' => $input['email'],
+            ->validate();
+
+        return DB::transaction(function () use ($input) { return tap(User::create([
+                'name' => $input['name'], 'email' => $input['email'],
                 'password' => Hash::make($input['password']),
             ]), function (User $user) {
                 $this->createTeam($user);
